401ce12580037c9b0aa689f239e6212972a32e5b
majaliju <majalijunyc@gmail.com>
2022-08-27 22:48:55 -0400

    tailwind working

---
+++ client/src/components/App.js
@@ -0,0 +1,21 @@
+import '../../src/App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <h1 className='text-5xl font-semibold italic'>Hello world!</h1>
+        <p>Mashlindo testing React with Tailwind here</p>
+        <a
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'>
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
