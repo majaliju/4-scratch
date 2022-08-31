52dd3b41e806bc24b712c006b3f63802fccf4095
majaliju <majalijunyc@gmail.com>
2022-08-30 22:17:01 -0400

    updated migration

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -3,5 +3,7 @@ class Concert < ApplicationRecord
   belongs_to :artist
   has_many :tickets
   has_many :posts
+  # not sure if the bottom two are valid 
   # has_many :genres, through: :artists
+  # has_many :users, through: :posts
 end
