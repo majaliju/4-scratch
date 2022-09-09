97b79dc3950a7b34c329d736fa4df6317473ffea
majaliju <majalijunyc@gmail.com>
2022-09-08 20:37:42 -0400

    added search bar

--- client/src/components/ArtistsDisplay.js
+++ client/src/components/ArtistsDisplay.js
@@ -2,11 +2,23 @@
 //! USE A FILTER DISPLAY FOR THE SEARCH
 //! ALSO IMPLEMENT A FILTER BY GENRE BUTTON SECTION (ALL - HIPHOP - RAP, ETC selector)
 
+//& make the search bar center itself -- check tailwind to find the right class
+
 function ArtistsDisplay({ artists }) {
   return (
     <div class='bg-base-900 py-6 sm:py-8 lg:py-12'>
       <div class='mx-auto max-w-screen-xl px-4 md:px-8'>
         <div class='mb-10 md:mb-16'>
+          <div class='form-control'>
+            <label class='input-group input-group-lg'>
+              <span>SEARCH</span>
+              <input
+                type='text'
+                placeholder='Search for your favorite artists here...just start typing'
+                class='input input-bordered input-lg'
+              />
+            </label>
+          </div>
           <h1 class='mb-4 text-center text-6xl font-thin text-primary md:mb-6 lg:text-7xl'>
             ARTISTS
           </h1>
