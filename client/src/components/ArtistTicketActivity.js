0f7f3ee4584d6146afc008470947997c902bc745
majaliju <majalijunyc@gmail.com>
2022-09-11 20:58:13 -0400

    working on the individual artist page

--- client/src/components/ArtistTicketActivity.js
+++ client/src/components/ArtistTicketActivity.js
@@ -1,7 +1,34 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
 function ArtistTicketActivity({ posts, setPosts, artists, concerts }) {
+  console.log(
+    "🚀 ~ file: ArtistTicketActivity.js ~ line 5 ~ ArtistTicketActivity ~ artists",
+    artists
+  );
+
+  let params = useParams();
+  console.log(
+    "🚀 ~ file: ArtistTicketActivity.js ~ line 8 ~ ArtistTicketActivity ~ params",
+    params
+  );
+
+  const thisArtist = artists.find(
+    (artist) => parseInt(params.id) === artist.id
+  );
+  console.log(
+    "🚀 ~ file: ArtistTicketActivity.js ~ line 12 ~ ArtistTicketActivity ~ thisArtist",
+    thisArtist
+  );
+
   return (
     <div>
-      <h1>{artists.map((artist) => artist.name)}</h1>
+      <h1>This is the artist page for {params.id}</h1>
+      <h2>Artist.id = {params.id}</h2>
+      <div>
+        <h1>{thisArtist.name}</h1>
+        <h1>{thisArtist.image}</h1>
+      </div>
     </div>
   );
 }
