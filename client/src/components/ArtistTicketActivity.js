622f110c9d3ae6377f0f743f319f1b579b9a02af
majaliju <majalijunyc@gmail.com>
2022-09-11 21:05:12 -0400

    individual cards in artist cards

--- client/src/components/ArtistTicketActivity.js
+++ client/src/components/ArtistTicketActivity.js
@@ -2,32 +2,24 @@ import React from "react";
 import { useParams } from "react-router-dom";
 
 function ArtistTicketActivity({ posts, setPosts, artists, concerts }) {
-  console.log(
-    "🚀 ~ file: ArtistTicketActivity.js ~ line 5 ~ ArtistTicketActivity ~ artists",
-    artists
-  );
+  let { id } = useParams();
 
-  let params = useParams();
-  console.log(
-    "🚀 ~ file: ArtistTicketActivity.js ~ line 8 ~ ArtistTicketActivity ~ params",
-    params
-  );
-
-  const thisArtist = artists.find(
-    (artist) => parseInt(params.id) === artist.id
-  );
-  console.log(
-    "🚀 ~ file: ArtistTicketActivity.js ~ line 12 ~ ArtistTicketActivity ~ thisArtist",
-    thisArtist
-  );
+  const artist = artists.find((artist) => parseInt(id) === artist.id);
 
   return (
     <div>
-      <h1>This is the artist page for {params.id}</h1>
-      <h2>Artist.id = {params.id}</h2>
-      <div>
-        <h1>{thisArtist.name}</h1>
-        <h1>{thisArtist.image}</h1>
+      <div class="card w-96 bg-base-100 shadow-xl">
+        <figure class="px-10 pt-10">
+          <img src={artist.image} alt={artist.name} class="rounded-xl" />
+        </figure>
+        <div class="card-body items-center text-center">
+          <h2 class="card-title">{artist.name}</h2>
+          <p>There's X upcoming concerts listed for {artist.name}!</p>
+          <div class="card-actions">
+            <button class="btn btn-primary">I have tickets to sell</button>
+            <button class="btn btn-primary">I'm Looking For Tickets</button>
+          </div>
+        </div>
       </div>
     </div>
   );
