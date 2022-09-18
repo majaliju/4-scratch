3e3baac2d36fba83c1d6b58cac79843ed2990ace
majaliju <majalijunyc@gmail.com>
2022-09-17 22:12:19 -0400

    createPost being worked on

--- client/src/components/CreatePost.js
+++ client/src/components/CreatePost.js
@@ -8,76 +8,21 @@ function CreatePost() {
           </h1>
 
           <form action='' class='p-8 mt-6 mb-0 rounded-lg shadow-2xl space-y-4'>
-            <p class='text-lg font-lg'>T</p>
-
             <div>
-              <label for='body' class='text-sm font-medium'>
-                body
-              </label>
-
-              <div class='relative mt-1'>
-                <input
-                  type='body'
-                  id='body'
-                  class='w-full p-4 pr-12 text-sm border-gray-200 rounded-lg shadow-sm'
-                  placeholder='Type out your post here!'
-                />
-
-                <span class='absolute inset-y-0 inline-flex items-center right-4'>
-                  <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    class='w-5 h-5 text-gray-400'
-                    fill='none'
-                    viewBox='0 0 24 24'
-                    stroke='currentColor'>
-                    <path
-                      stroke-linecap='round'
-                      stroke-linejoin='round'
-                      stroke-width='2'
-                      d='M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207'
-                    />
-                  </svg>
-                </span>
-              </div>
+              <input
+                type='text'
+                placeholder='what price? best offer only? write all that here!'
+                class='input input-ghost w-full '
+              />
             </div>
 
-            <div>
-              {/* GOTTA DO A CHECKBOX IF LOOKING FOR SELLING -- INFO COMES FROM WHAT THE USER PASSED IN BASED ON WHICH BUTTON THEY CLICK */}
-              <label for='for_sale' class='text-sm font-medium'>
-                for_sale
-              </label>
-
-              <div class='relative mt-1'>
-                <input
-                  type='for_sale'
-                  id='for_sale'
-                  class='w-full p-4 pr-12 text-sm border-gray-200 rounded-lg shadow-sm'
-                  placeholder='Enter for_sale'
-                />
-
-                <span class='absolute inset-y-0 inline-flex items-center right-4'>
-                  <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    class='w-5 h-5 text-gray-400'
-                    fill='none'
-                    viewBox='0 0 24 24'
-                    stroke='currentColor'>
-                    <path
-                      stroke-linecap='round'
-                      stroke-linejoin='round'
-                      stroke-width='2'
-                      d='M15 12a3 3 0 11-6 0 3 3 0 016 0z'
-                    />
-                    <path
-                      stroke-linecap='round'
-                      stroke-linejoin='round'
-                      stroke-width='2'
-                      d='M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z'
-                    />
-                  </svg>
-                </span>
-              </div>
-            </div>
+            <select class='select select-ghost w-full '>
+              <option disabled selected>
+                Are you selling or requesting?
+              </option>
+              <option>I'm Selling Tickets</option>
+              <option>I'm Requesting Tickets</option>
+            </select>
 
             <button
               type='submit'
