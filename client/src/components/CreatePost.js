081d340825e4d4a618d71de77983cdea2d987975
majaliju <majalijunyc@gmail.com>
2022-09-19 22:39:04 -0400

    edits on CreatePost

--- client/src/components/CreatePost.js
+++ client/src/components/CreatePost.js
@@ -8,6 +8,42 @@ function CreatePost() {
   const [ticketAmount, setTicketAmount] = useState('');
   const [error, setError] = useState('');
 
+  function checkError(response) {
+    if (response.status >= 200 && response.status <= 299) {
+      return response.json();
+    } else {
+      console.log('response: ', response);
+      console.log('response.status: ', response.status);
+      console.log('response.statusText: ', response.statusText);
+      throw response;
+    }
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log('within the handleSubmit for form: ', e);
+    // fetch('/new_post', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //     'Access-Control-Allow-Origin': '*',
+    //   },
+    //   body: JSON.stringify({
+    //     body,
+    //     for_sale: selling,
+    //     how_many_tickets: ticketAmount,
+    //     concert_id: null,
+    //     user_id: null,
+    //   }),
+    // })
+    //   .then(checkError)
+    //   .then((item) => {
+    //     console.log(item);
+    //   })
+    //   .catch((err) => console.log(err));
+    // navigate(-1);
+  }
+
   return (
     <div>
       <div class='px-4 py-16 mx-auto max-w-screen-xl sm:px-6 lg:px-8'>
@@ -16,7 +52,7 @@ function CreatePost() {
             Easily post your tickets!
           </h1>
 
-          <form action='' class='p-8 mt-6 mb-0 rounded-lg shadow-2xl space-y-4'>
+          <form class='p-8 mt-6 mb-0 rounded-lg shadow-2xl space-y-4'>
             <select class='select select-ghost w-full '>
               <option disabled selected>
                 Are you selling or requesting?
@@ -27,6 +63,8 @@ function CreatePost() {
             <div>
               <input
                 type='text'
+                id='selling'
+                value={selling}
                 placeholder='what price? best offer only? write all that here!'
                 class='input input-ghost w-full '
               />
@@ -41,6 +79,7 @@ function CreatePost() {
             </div>
 
             <button
+              onClick={handleSubmit}
               type='submit'
               class='block w-full px-5 py-3 text-sm font-medium text-white bg-indigo-600 rounded-lg'>
               Submit
