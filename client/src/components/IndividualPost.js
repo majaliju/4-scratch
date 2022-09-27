65f86c8157af5baef70cd4ea47517bc3fda9ca8e
majaliju <majalijunyc@gmail.com>
2022-09-27 19:47:37 -0400

    gradient border on posts

--- client/src/components/IndividualPost.js
+++ client/src/components/IndividualPost.js
@@ -16,9 +16,9 @@ function IndividualPost({ eachPost, posts }) {
 
   return (
     <div class='p-1 shadow-xl bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 rounded-2xl'>
-      <div class='block p-2 sm:p-4 rounded-xl'>
-        <div class='m-2 sm:pr-4'>
-          <h2 class='text-xl font-bold text-gray-900'>
+      <div class='block p-6 bg-white sm:p-8 rounded-xl'>
+        <div class='sm:pr-8'>
+          <h2 class='text-xl font-bold text-center text-gray-900'>
             {thisPost.for_sale === true ? (
               <h1 class='text-3xl justify-center'>
                 SELLING {thisPost.how_many_tickets} TICKETS
@@ -29,16 +29,9 @@ function IndividualPost({ eachPost, posts }) {
               </span>
             )}
           </h2>
-          <h2 class='mt-2 text-lg text-white'>{thisPost.body}</h2>
+
+          <p class='mt-2 text-sm text-center  text-gray-500'>{thisPost.body}</p>
         </div>
-        <h4 class='mt-2 text-lg text-white'>EMAIL: {thisPost.user.email}</h4>
-        <h4 class='mt-2 text-lg text-white'>
-          <button
-            class='btn btn-ghost btn-outline outline-black'
-            onClick={() => navigate(`/concerts/${thisPost.concert.id}`)}>
-            SEE CONCERT INFO
-          </button>
-        </h4>
       </div>
     </div>
   );
@@ -46,5 +39,32 @@ function IndividualPost({ eachPost, posts }) {
 
 export default IndividualPost;
 
-// // //* shadow with gradient border
-// // //* from https://www.hyperui.dev/components/marketing/cards
+{
+  //* the original
+  /* <div class='p-1 shadow-xl bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 rounded-2xl'>
+  <div class='block p-2 sm:p-4 rounded-xl'>
+    <div class='m-2 sm:pr-4'>
+      <h2 class='text-xl font-bold text-gray-900'>
+        {thisPost.for_sale === true ? (
+          <h1 class='text-3xl justify-center'>
+            SELLING {thisPost.how_many_tickets} TICKETS
+          </h1>
+        ) : (
+          <span class='text-3xl justify-center'>
+            LOOKING FOR {thisPost.how_many_tickets} TICKETS
+          </span>
+        )}
+      </h2>
+      <h2 class='mt-2 text-lg text-white'>{thisPost.body}</h2>
+    </div>
+    <h4 class='mt-2 text-lg text-white'>EMAIL: {thisPost.user.email}</h4>
+    <h4 class='mt-2 text-lg text-white'>
+      <button
+        class='btn btn-ghost btn-outline outline-black'
+        onClick={() => navigate(`/concerts/${thisPost.concert.id}`)}>
+        SEE CONCERT INFO
+      </button>
+    </h4>
+  </div>
+</div>; */
+}
